268e0cb7dd4a791f27dd80fefdc10ba4f9665c20
Agustín Pensotti <91265440+tottipensotti@users.noreply.github.com>
2023-11-22 09:20:53 -0300

    Update albums_transform.py

--- src/etl/transform/albums_transform.py
+++ src/etl/transform/albums_transform.py
@@ -20,32 +20,31 @@ def transformTrackData(songs):
     columns=['id','name', 'artist_id', 'album_type', 'release_date',
     'total_tracks', 'available_markets','disc_number']
 
-    def getArtists(i):
-        if len(songs['items'][i]['track']['album']['artists'])>1:
-            artist = []
-            for a in range(0,len(songs['items'][i]['track']['album']['artists'])):
-                artist.append(songs['items'][i]['track']['album']['artists'][a]['name'])
-                return artist
+    def getArtists(album):
+        if len(album['artists']) > 1:
+            return ', '.join([artist['name'] for artist in album['artists']])
         else:
-            return songs['items'][i]['track']['album']['artists'][a]['name']
+            return album['artists'][0]['name']
     
     try:
-        for i in range(0,len(songs['items'])):
-            tracks.append([
-                songs['items'][i]['track']['album']['id'],
-                songs['items'][i]['track']['album']['name'],
-                getArtists(i),
-                songs['items'][i]['track']['album']['album_type'],
-                songs['items'][i]['track']['album']['release_date'],
-                songs['items'][i]['track']['album']['total_tracks'],
-                ', '.join(songs['items'][i]['track']['available_markets']),
-                songs['items'][i]['track']['album']['disc_number']
-            ])
+        for i in range(0, len(songs['items'])):
+        tracks.append([
+            songs['items'][i]['track']['album']['id'],
+            songs['items'][i]['track']['album']['name'],
+            getArtists(songs['items'][i]['track']['album']),
+            songs['items'][i]['track']['album']['album_type'],
+            songs['items'][i]['track']['album']['release_date'],
+            songs['items'][i]['track']['album']['total_tracks'],
+            ', '.join(songs['items'][i]['track']['available_markets']),
+            songs['items'][i]['track']['album']['disc_number']
+        ])
+        
         dm_track = pd.DataFrame(tracks, columns=columns)
-        print('Successfuly transformed tracks data')
+        print('Successfully transformed tracks data')
         return dm_track
-    except:
-        print('Error while transforming tracks data.')
+    
+    except Exception as e:
+        print(f'Error while transforming tracks data: {e}')
 
 if __name__ == '__main__':
     transformTrackData(songs)
